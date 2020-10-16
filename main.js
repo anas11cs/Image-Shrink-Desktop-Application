e14f7995a39cbcab32f388e790c5163afaeb6a65
anas11cs <annus.waheed01@gmail.com>
2020-10-16 21:36:15 +0500

    Window Popped

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 //console.log('Hello World');
 const {app, BrowserWindow} = require('electron');
 // Here curly braces specifies that after requiring, what we need
-// to fetch out of electron
+// to fetch out of electron by de-structuring
+// 'app' actually manages entire life cycle of our app
+// 'BrowserWindow' is a class that is used to create a Desktop Window
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title:'Image Shrink',
+        width:500,
+        height:600
+    });
+}
+app.on('ready',createMainWindow);
