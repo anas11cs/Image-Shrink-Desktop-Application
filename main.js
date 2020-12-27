125c16f02d39c329040c8ff78cf27146b6986a81
anas11cs <annus.waheed01@gmail.com>
2020-12-27 08:23:01 +0500

    Added Image Resizing of .png and .jpeg with some more features/functionalities(Last Commit Before Packaging App)

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 // console.log('Hello World');
-const {app, BrowserWindow, Menu/*, globalShortcut*/} = require('electron'); // Node Js module syntax
+const path = require('path')
+const os = require('os')
+const {app, BrowserWindow, Menu, ipcMain, shell/*, globalShortcut*/} = require('electron'); // Node Js module syntax
 // Here curly braces specifies that after requiring, what we need
 // to fetch out of electron by de-structuring
 // 'app' actually manages entire life cycle of our app
 // 'BrowserWindow' is a class that is used to create a Desktop Window
-
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
+//https://www.npmjs.com/package/electron-log
+// In Windows Log appears at => %USERPROFILE%\AppData\Roaming\{app name}\logs\{process type}.log
+const log = require('electron-log')
 // Set Environment
-process.env.NODE_ENV = 'development'; // Currently, Electron in "Development" phase
+process.env.NODE_ENV = 'production'; // Currently, Electron in "Development" phase
 const isDev = process.env.NODE_ENV !== 'production' ? true : false; // if(process.env.NODE_ENV !== 'production'){isDev=true;}else{isDev=false;}
 const isMac = process.platform === 'darwin' ? true : false;
 // Windows Used Below
@@ -20,12 +28,18 @@ function createMainWindow(){
              nodeIntegration:true
           },
         title:'Image Shrink',
-        width:500,
+        width: isDev ? 800 : 500,
         height:600,
         icon: `${__dirname}/assets/icons/icons/Icon_256x256.png`,
         resizable: isDev,
-        backgroundColor:'white'
+        backgroundColor:'white',
+        webPreferences:{
+            nodeIntegration:true,
+        },
     })
+    // if(isDev){
+    //     mainWindow.webContents.openDevTools()
+    // }
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     // KEEP IN MIND SYNTAX IS(back tick) ` `, not ' ' (not Commas)
 }
@@ -128,6 +142,33 @@ const menu =[
     ),
 ]
 
+ipcMain.on('image:minimize', (e,options)=> {
+    options.dest = path.join(os.homedir(), '/Desktop/ImageShrinkTestFolder')
+    shrinkImage(options)
+})
+async function shrinkImage({imgPath, quality, dest}){
+    try{
+        const pngQuality = quality / 100
+        const files = await imagemin([slash(imgPath)],{
+            destination: dest,
+            plugins: [
+                imageminMozjpeg({quality}),
+                imageminPngquant({
+                    quality:[pngQuality, pngQuality]
+                }),
+            ],
+        })
+        //console.log(files)
+        log.info(files)
+        shell.openPath(dest)
+        mainWindow.webContents.send('image:done')
+        // It can be ipcMain.send or ipcMain.reply
+    }
+    catch(err){
+        //console.log(err)
+        log.error(err)
+    }
+}
 // Mac Configurations
 app.on('window-all-closed', () => {
   if (!isMac) {
